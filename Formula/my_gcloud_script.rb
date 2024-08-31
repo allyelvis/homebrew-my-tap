f4806dc2fa775513097456e59c360eb6a342f75d
Ally Elvis Nzeyimana <allyelvis6569@gmail.com>
2024-09-01 01:55:37 +0200

    Update my_gcloud_script.rb

--- Formula/my_gcloud_script.rb
+++ Formula/my_gcloud_script.rb
@@ -2,7 +2,7 @@ class MyGcloudScript < Formula
   desc "Google Cloud User Management Script"
   homepage "https://github.com/allyelvis/my-gcloud-script"
   url "https://github.com/allyelvis/my-gcloud-script/archive/v1.0.0.tar.gz"
-  sha256 "hash_of_the_tar_file"
+  sha256 "shasum -a 256 path/to/yourfile.tar.gz"
 
   def install
     bin.install "manage_gcloud_user.sh"
